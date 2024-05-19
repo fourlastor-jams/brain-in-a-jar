4da3ada091d962c40e68e3ad378219617d49eeba
Daniele Conti <fourlastor@users.noreply.github.com>
2024-05-19 20:58:51 +0200

    Remove unused stuff

--- core/src/main/java/io/github/fourlastor/game/GdxGame.java
+++ core/src/main/java/io/github/fourlastor/game/GdxGame.java
@@ -3,33 +3,27 @@ package io.github.fourlastor.game;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
-import com.badlogic.gdx.Screen;
 import com.github.tommyettinger.ds.ObjectList;
 import io.github.fourlastor.game.di.GameComponent;
 import io.github.fourlastor.game.intro.IntroComponent;
-import io.github.fourlastor.game.level.di.LevelComponent;
-import io.github.fourlastor.game.route.Router;
 import io.github.fourlastor.harlequin.Harlequin;
-import java.util.List;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.List;
 
 @Singleton
-public class GdxGame extends Game implements Router {
+public class GdxGame extends Game {
 
     private final InputMultiplexer multiplexer;
 
-    private final LevelComponent.Builder levelScreenFactory;
     private final IntroComponent.Builder introScreenFactory;
-    private Screen pendingScreen = null;
 
     @Inject
     public GdxGame(
             InputMultiplexer multiplexer,
-            LevelComponent.Builder levelScreenFactory,
             IntroComponent.Builder introScreenFactory) {
         this.multiplexer = multiplexer;
-        this.levelScreenFactory = levelScreenFactory;
         this.introScreenFactory = introScreenFactory;
         Harlequin.LIST_CREATOR = new Harlequin.ListCreator() {
             @Override
@@ -46,37 +40,11 @@ public class GdxGame extends Game implements Router {
 
     @Override
     public void create() {
-        //        if (Gdx.app.getType() != Application.ApplicationType.Android) {
-        //
-        //            Cursor customCursor =
-        //                    Gdx.graphics.newCursor(new Pixmap(Gdx.files.internal("images/included/whitePixel.png")),
-        // 0, 0);
-        //            Gdx.graphics.setCursor(customCursor);
-        //        }
         Gdx.input.setInputProcessor(multiplexer);
-        goToIntro();
-    }
-
-    @Override
-    public void render() {
-        if (pendingScreen != null) {
-            setScreen(pendingScreen);
-            pendingScreen = null;
-        }
-        super.render();
+        setScreen(introScreenFactory.build().screen());
     }
 
     public static GdxGame createGame() {
         return GameComponent.component().game();
-    }
-
-    @Override
-    public void goToIntro() {
-        pendingScreen = introScreenFactory.router(this).build().screen();
-    }
-
-    @Override
-    public void goToLevel() {
-        pendingScreen = levelScreenFactory.router(this).build().screen();
     }
 }
--- core/src/main/java/io/github/fourlastor/game/di/modules/ScreensModule.java
+++ core/src/main/java/io/github/fourlastor/game/di/modules/ScreensModule.java
@@ -2,11 +2,10 @@ package io.github.fourlastor.game.di.modules;
 
 import dagger.Module;
 import io.github.fourlastor.game.intro.IntroComponent;
-import io.github.fourlastor.game.level.di.LevelComponent;
 
 @Module(
         subcomponents = {
-            LevelComponent.class,
-            IntroComponent.class,
+                IntroComponent.class,
         })
-public class ScreensModule {}
+public class ScreensModule {
+}
